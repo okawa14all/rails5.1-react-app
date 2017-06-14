8c91785ac77b8e847049e83a417e31c892efe2e9
Hitoshi Okawa <14all.okawa@gmail.com>
2017-06-14 17:26:58 +0900

    Add TasksController

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,11 @@ module Rails51ReactApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.system_tests = nil
+    end
   end
 end
