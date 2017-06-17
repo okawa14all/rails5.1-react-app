545fecebf75b4c4234f3d152206ac6e224a4e4e9
Hitoshi Okawa <14all.okawa@gmail.com>
2017-06-17 10:11:08 +0900

    Add todos#create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,22 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    respond_to do |format|
+      format.html {
+        redirect_to root_path
+      }
+      format.json {
+        head 204
+      }
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:text)
+  end
 end
